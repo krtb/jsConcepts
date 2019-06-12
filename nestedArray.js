20cff6ae144c10494d2bc26505021dad611e0f54
Kurt Bauer <kurtbauer55@protonmail.com>
2019-06-12 19:23:15 -0400

    second example for accessing nested arrays

--- nestedArray.js
+++ nestedArray.js
@@ -18,3 +18,29 @@ var ourPets = [
 ];
 ourPets[0].names[1]; // "Fluffy"
 ourPets[1].names[0]; // "Spot"
+
+// TODO:EXAMPLE 2
+
+// Setup
+var myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
